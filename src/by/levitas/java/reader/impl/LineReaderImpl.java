578fd5ce0f04f93465b2aa3c820f5cfc48d643b6
Ilya <levitas.ilya@mail.ru>
2022-02-09 18:04:15 +0300

    final

--- src/by/levitas/java/reader/impl/LineReaderImpl.java
+++ src/by/levitas/java/reader/impl/LineReaderImpl.java
@@ -12,10 +12,9 @@ public class LineReaderImpl {
             File file = new File(path);
             FileReader fileReader = new FileReader(file);
             BufferedReader reader = new BufferedReader(fileReader);
-            String line = reader.readLine();
-            while (line != null) {
+            String line;
+            while ((line = reader.readLine()) != null) {
                 readenInfo.add(line);
-                line = reader.readLine();
             }
         } catch (FileNotFoundException e) {
             throw new CustomException(e);
--- src/by/levitas/java/validator/ValidateString.java
+++ src/by/levitas/java/validator/ValidateString.java
@@ -4,8 +4,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ValidateString {
+    private static final String VALIDATION_PATTERN = "^[-0-9 ]+$";
+
     public static boolean IsContainOnlyNumbers(String stringToValidate) {
-        Pattern pattern = Pattern.compile("^[-0-9 ]+$");
+        Pattern pattern = Pattern.compile(VALIDATION_PATTERN);
         Matcher matcher = pattern.matcher(stringToValidate);
 
         return matcher.matches();
