406248f86add2291b742088426dcd4e0eae3b24d
Ilya <levitas.ilya@mail.ru>
2022-02-09 04:18:49 +0300

    test commit

--- src/by.levitas/java/reader/LineReaderInterface.java
+++ src/by.levitas/java/reader/LineReaderInterface.java
@@ -3,5 +3,5 @@ package reader;
 import java.util.ArrayList;
 
 public interface LineReaderInterface {
-    public ArrayList<String> readFile(String path);
+    ArrayList<String> readFile(String path);
 }
